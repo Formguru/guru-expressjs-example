cc52bfbcccf0e0b59264b1f2bcf2152a7f0f0bde
Rick Bhardwaj <rickbhardwaj@gmail.com>
2023-10-27 11:36:47 -0400

    Copying over sample Express.js Guru app to public repo (#1)
    * Copying over sample Express.js Guru app
    
    * README updates

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# guru-expressjs-example
-Example Express.js app showing how to use the Guru API
+# Guru API Integration with Express.js
+
+## Introduction
+
+This repository provides an example Express.js application for integrating Guru's API into your server-side code. It demonstrates how to obtain an access token, upload a video, and fetch video analysis using the Guru API.
+
+### Analyze Video API
+
+This API allows you perform AI analysis on a video by uploading it to the Guru API.
+
+#### Prerequisites
+
+- Node.js & npm installed
+- A Guru API client ID and client secret. To find yours, go to https://console.getguru.ai/
+- A .env file in the root of your project with the following content:
+
+```bash
+GURU_CLIENT_ID=YOUR_CLIENT_ID_HERE
+GURU_CLIENT_SECRET=YOUR_CLIENT_SECRET_HERE
+```
+
+- Or set envronment variables:
+
+```bash
+export GURU_CLIENT_ID=your_client_id
+export GURU_CLIENT_SECRET=your_client_secret
+```
+
+- Add a test video to `/videos`
+
+#### Setup
+
+```bash
+# Install the required pacakges
+npm install
+# Start the server
+node index.js
+```
+
+#### Testing from the CLI
+
+To test the `/analyze-video` endpoint from the command line, you can use tools like `curl`.
+
+Here's how you can do it:
+
+- Make sure the video file `test.mp4` is present in `/videos`.
+- Run the following command:
+
+```bash
+curl -X POST http://localhost:3000/analyze-video
+```
+
+This should initiate the process of fetching a token, uploading the video, and getting its analysis. The server will poll for analysis for up to 5 minutes.
+
+If successful, you'll receive a JSON response with the analysis data. If there's an error, it will return an appropriate error message.
+
+For support, reach out to support@getguru.ai or join the Guru Discord server.
+
+### License
+
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+Happy coding!
---
+++ index.js
@@ -0,0 +1,125 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const fs = require("fs");
+const FormData = require("form-data");
+const app = express();
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+// Endpoint for analyzing a video
+app.post("/analyze-video", async (req, res) => {
+  try {
+    console.log("Fetching token...");
+    const authToken = await getToken();
+    console.log("Fetched token.");
+
+    console.log("Uploading video...");
+    const videoId = await uploadVideo(authToken);
+    console.log(`Uploaded video ${videoId}.`);
+
+    console.log(`Fetching video analysis for video ${videoId}...`);
+    const analysisData = await getAnalysis(videoId, authToken);
+
+    res.json(analysisData);
+  } catch (error) {
+    console.error("Error in /analyze-video:", error.message);
+    next(error);
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: err.message });
+});
+
+// Function to get an access token
+const getToken = async () => {
+  if (fs.existsSync("token.json")) {
+    const tokenData = JSON.parse(fs.readFileSync("token.json", "utf8"));
+    if (Date.now() < tokenData.expiry) {
+      return tokenData.token; // return existing token if it's still valid
+    }
+  }
+
+  const client_id = process.env.GURU_CLIENT_ID;
+  const client_secret = process.env.GURU_CLIENT_SECRET;
+
+  const options = {
+    method: "POST",
+    url: "https://customer-console-prod.auth.us-west-2.amazoncognito.com/oauth2/token",
+    headers: { "content-type": "application/x-www-form-urlencoded" },
+    data: `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}&scope=https://api.getguru.fitness/default`,
+  };
+
+  const response = await axios(options);
+  const tokenData = {
+    token: response.data.access_token,
+    expiry: Date.now() + response.data.expires_in * 1000,
+  };
+  fs.writeFileSync("token.json", JSON.stringify(tokenData));
+  return tokenData.token;
+};
+
+// Function to upload a video
+const uploadVideo = async (authToken) => {
+  const videoPath = "./videos/test.mp4";
+  const videoStream = fs.createReadStream(videoPath);
+  const videoSizeInBytes = fs.statSync(videoPath).size;
+  const videoData = {
+    filename: "test_squat.mp4",
+    size: videoSizeInBytes,
+    schemaId: "1569597a-3b59-45bf-9048-7287be905f48",
+    source: "guru-api-test",
+  };
+
+  const createVideoResponse = await axios({
+    method: "post",
+    url: "https://api.getguru.fitness/videos",
+    headers: { Authorization: authToken },
+    data: videoData,
+  });
+
+  const formData = new FormData();
+  Object.keys(createVideoResponse.data.fields).forEach((key) => {
+    formData.append(key, createVideoResponse.data.fields[key]);
+  });
+  formData.append("file", videoStream, "test_squat.mp4");
+
+  await axios.post(createVideoResponse.data.url, formData, {
+    headers: { ...formData.getHeaders() },
+  });
+
+  return createVideoResponse.data.id;
+};
+
+// Function to get video analysis
+const getAnalysis = async (videoId, authToken) => {
+  const endTime = Date.now() + 5 * 60 * 1000; // 5 minutes from now
+
+  while (Date.now() < endTime) {
+    try {
+      const response = await axios({
+        url: `https://api.getguru.fitness/videos/${videoId}/analysis`,
+        headers: { Authorization: authToken },
+      });
+
+      if (response.data.status && response.data.status === "Pending") {
+        console.log("Polling for video analysis...");
+        await new Promise((resolve) => setTimeout(resolve, 5000)); // Poll every 5 seconds
+      } else {
+        return response.data;
+      }
+    } catch (error) {
+      console.error("Error during polling for video analysis:", error.message);
+    }
+  }
+
+  throw new Error("Video analysis did not complete in the expected time.");
+};
